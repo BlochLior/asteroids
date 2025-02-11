f3670eb445932922a8abeb6009b00f965cbacae0
BlochLior <blochlior@gmail.com>
2025-02-11 21:40:06 +0200

    added the ability to rotate the player

--- main.py
+++ main.py
@@ -11,16 +11,17 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
-        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,)
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        print(dt)
+
 
 if __name__ == "__main__":
     main()
